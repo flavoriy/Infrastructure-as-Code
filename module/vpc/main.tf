05da90cc64322a3fb906007f17f185473f20e71a
PhanManhTan <manhtan06120@gmail.com>
2026-07-14 14:16:29 +0700

    fix: restore checkov skip comments for VPC and Public Subnet

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,3 +1,4 @@
+#checkov:skip=CKV2_AWS_11:VPC Flow Logs not required for this DevOps lab environment
 resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   tags = {
@@ -37,6 +38,7 @@ resource "aws_subnet" "prod" {
 }
 
 resource "aws_subnet" "public" {
+  #checkov:skip=CKV_AWS_130:Public subnets must map public IPs on launch by design to allow public resources to be reachable
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
